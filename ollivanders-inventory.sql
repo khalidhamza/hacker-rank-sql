a887bcfcd56366e6bef99cda4a8580880a022a9d
Khalid Hamza <khalid_hamza2015@yahoo.com>
2023-07-19 18:23:49 +0300

    solve Revising the Select Query I

--- ollivanders-inventory.sql
+++ ollivanders-inventory.sql
@@ -1,5 +1,6 @@
 /*
     Problem (Ollivander's Inventory)
+    
     Harry Potter and his friends are at Ollivander's with Ron, finally replacing Charlie's old broken wand.
     Hermione decides the best way to choose is by determining the minimum number of gold galleons needed to buy each non-evil wand of high power and age. Write a query to print the id, age, coins_needed, and power of the wands that Ron's interested in, sorted in order of descending power. If more than one wand has same power, sort the result in order of descending age.
 */
