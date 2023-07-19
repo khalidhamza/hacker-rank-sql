bdb9e087f5cfcb6e2196dfc05535a84530a96d31
Khalid Hamza <khalid_hamza2015@yahoo.com>
2023-07-19 17:44:04 +0300

    slove Japanese Cities

---
+++ japanese-cities-attributes.sql
@@ -0,0 +1,6 @@
+/*
+    Problem (Japanese Cities' Attributes)
+
+    Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+*/
+SELECT * FROM CITY WHERE  COUNTRYCODE = 'JPN';
---
+++ select-by-id.sql
@@ -0,0 +1,6 @@
+/*
+    Problem (Select By ID)
+
+    Query all columns (attributes) for every row in the CITY table.
+*/
+SELECT * FROM CITY WHERE ID = 1661;
