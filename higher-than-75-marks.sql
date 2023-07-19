7c60a3fb93f493a3a7c26c7090f98803122b013f
Khalid Hamza <khalid_hamza2015@yahoo.com>
2023-07-19 18:31:43 +0300

    solve Higher Than 75 Marks

---
+++ higher-than-75-marks.sql
@@ -0,0 +1,6 @@
+/*
+    Problem (Higher Than 75 Marks)
+
+    Query the Name of any student in STUDENTS who scored higher than 75 Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+*/
+SELECT Name FROM STUDENTS WHERE Marks > 75 ORDER BY RIGHT(Name, 3), ID;
